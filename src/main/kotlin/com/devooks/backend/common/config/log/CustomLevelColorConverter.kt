ec949589f43d5a298400ec3a6ecd16e09643de2e
Sangmin Lee <nalsm98@naver.com>
2024-11-19 16:37:46 +0900

    [DVK-97] chore: 로깅 설정

---
+++ src/main/kotlin/com/devooks/backend/common/config/log/CustomLevelColorConverter.kt
@@ -0,0 +1,27 @@
+package com.devooks.backend.common.config.log
+
+import ch.qos.logback.classic.Level
+import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.core.pattern.color.ForegroundCompositeConverterBase
+
+class CustomLevelColorConverter : ForegroundCompositeConverterBase<ILoggingEvent>() {
+
+    override fun getForegroundColorCode(event: ILoggingEvent): String? =
+        when (event.level) {
+            Level.ERROR -> RED
+            Level.WARN -> YELLOW
+            Level.DEBUG -> MAGENTA
+            Level.TRACE -> BLUE
+            Level.INFO -> GREEN
+            else -> WHITE
+        }.toString()
+
+    companion object {
+        private const val RED = "1;31"
+        private const val GREEN = "1;32"
+        private const val YELLOW = "1;33"
+        private const val BLUE = "1;34"
+        private const val MAGENTA = "1;35"
+        private const val WHITE = "1;37"
+    }
+}
